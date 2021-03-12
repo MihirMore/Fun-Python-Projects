63ae009c78e8e3c84bf69dd25c88599dd90de2e9
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-03-12 21:21:31 +0530

    Code completed

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -3,7 +3,7 @@ from words import words
 from hangman_visual import lives_visual_dict
 import string 
 
-def get_valid_word(words)
+def get_valid_word(words):
     word = random.choice(words)
     while "-" in words or " " in words:
         word = random.choice(words)
@@ -20,5 +20,39 @@ def hangman():
 
     while len(word_letters) > 0 and lives > 0:
         print("You have ", lives, "lives left and you have used these letters: "," ".join(used_letters))
+
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print(lives_visual_dict[lives])
+        print("Current word: ", " ".join(word_list))
+
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+                print('')
+
+            else:
+                lives = lives - 1  # takes away a life if wrong
+                print('\nYour letter,', user_letter, 'is not in the word.')
+
+        elif user_letter in used_letters:
+            print('\n You have already used that letter. Guess another letter.')
+
+        else:
+            print('\n That is not a valid letter.')
+
+            
+    if lives == 0:
+        print(lives_visual_dict[lives])
+        print('You died, sorry. The word was', word)
+    else:
+        print('YAY! Congrats, You guessed the word', word, '!!')
+
+
+if __name__ == '__main__':
+    hangman()
+
         
 
