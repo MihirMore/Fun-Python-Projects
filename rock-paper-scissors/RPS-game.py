e707ec218368cdfcc8a2f6213d4643953f8f827b
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-03-12 11:48:24 +0530

    Rock paper scissors code added

--- rock-paper-scissors/RPS-game.py
+++ rock-paper-scissors/RPS-game.py
@@ -1,1 +1,22 @@
+import random   
 
+def play():
+    user = input("What's your choice? Rock 'r', Paper 'p', Scissors 's': ").lower()
+    computer = random.choice(['r', 'p' , 's'])
+
+    print(f"Computer chose {computer}")
+
+    if user == computer:
+        return "It's a tie!"
+
+    if who_wins ( user, computer ):
+        return "You won!"
+
+    return "You lost!"
+
+def who_wins( player , opponent):
+
+    if( player == 'r' and opponent == 's' or player == 's' and opponent == 'p' or player == 'p' and opponent == 'r'):
+        return True
+
+print(play())
