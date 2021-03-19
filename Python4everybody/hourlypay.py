46a3b4318dc4879c16ad3a721a145532582f2d44
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-03-19 12:38:32 +0530

    Update hourlypay.py

--- Python4everybody/hourlypay.py
+++ Python4everybody/hourlypay.py
@@ -1,2 +1,5 @@
 hours = int(input("Please enter the number of hours: "))
-print(f"Number of hours {hours}")
+rate = int(input("Please enter the hourly rate: "))
+pay = hours * rate 
+
+print(f"Your pay is {pay}")
