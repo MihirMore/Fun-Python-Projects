5ac6c7ab72b5e57c33572c70d909a53434e21d72
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-03-22 12:54:56 +0530

    Update overtimepay.py

--- Python4everybody/overtimepay.py
+++ Python4everybody/overtimepay.py
@@ -1,5 +1,10 @@
-hoursWorked = float(input("Enter hours: "))
-ratePerHr = float(input("Enter rate/hr: "))
+try:
+    hoursWorked = float(input("Enter hours: "))
+    ratePerHr = float(input("Enter rate/hr: "))
+except:
+    print("Please enter a numeric value only")
+    quit()
+    
 regularPay = hoursWorked * ratePerHr
 if hoursWorked > 40.0:    
     overtimePay = regularPay + (hoursWorked - 40.0) * (ratePerHr * 0.5)
