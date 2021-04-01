bfc5f354ebd99115be48c65918e2af7e7bf88c84
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-01 23:41:31 +0530

    Update biography.py

--- Biography/biography.py
+++ Biography/biography.py
@@ -9,6 +9,16 @@ while True:
     except:
         print("Please enter a valid date")
         continue
+    try:
+        address = str(input("Please enter you address: "))
+    except:
+        print("Please enter a valid address")
+        continue
+    try:
+        personal_goals = str(input("What is your career goal?: "))
+    except:
+        print("Please enter a valid string")
+        continue                
     break
 
-print(f"- Name: {name} \n- Date of birth: {dob} \n ")                   
+print(f"- Name: {name} \n- Date of birth: {dob} \n- Address: {address}\n- Personal Goals: {personal_goals} ")                   
