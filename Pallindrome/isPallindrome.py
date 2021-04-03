a2c1ece9c0b72873dfd8b636103e2d21883f0e77
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-03 23:00:51 +0530

    Update isPallindrome.py

---
+++ Pallindrome/isPallindrome.py
@@ -0,0 +1,23 @@
+def convertInputString():
+    rawinput = input("\n Please enter a word, a phrase or a sentence to check if it's a pallindrome: ")
+    rawstring = rawinput.lower()
+    rawlist = list(rawstring)
+    return rawlist
+
+def dirtyaphabets(dirtyList):
+    dirtyAlphabetics = [" ", "-", ".", ",", ":", ";", "!", "?", "'", "\""]
+    for character in dirtyAlphabetics:
+        if character in dirtyList:
+            dirtyList.remove(character)
+            return dirtyaphabets(dirtyList)
+    return dirtyList
+
+def pallindromeCheck(cleanList):
+    reversedList = cleanList[::-1]
+    if reversedList == cleanList:
+        return "The text you entered is a Pallindrome!"
+    else:
+        return "The text you entered is not a Pallindrome."                
+
+def main():
+    print("\nPallindrome Checker")        
