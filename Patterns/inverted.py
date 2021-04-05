8b8b755073ee2244c8762b511f8a7850742106a5
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-06 01:10:16 +0530

    Update inverted.py

--- Patterns/inverted.py
+++ Patterns/inverted.py
@@ -1,7 +1,7 @@
 n = int(input("Please enter the number of rows: "))
 
 for rows in range( n + 1, 0, -1):
-    for columns in range(0 , n):
+    for columns in range(0 , rows - 1):
         print("*",end="")
-    n = n - 1    
+      
     print()    
