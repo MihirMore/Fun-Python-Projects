1260d3fca0fc280f45406d949237c892b8e973dc
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-05 18:49:14 +0530

    Update emailslicer.py

--- Emailslicer/emailslicer.py
+++ Emailslicer/emailslicer.py
@@ -2,6 +2,9 @@ email = input("Please enter your email id: ").strip()
 
 user_name = email[:email.index("@")]
 
-domain_name = email[email.index("@"):]
+domain_name = email[email.index("@")+1:]
 
+output = f"Your username is {user_name} and your domain name is {domain_name}"
+
+print(output)
 
