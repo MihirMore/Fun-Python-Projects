76b0e8cc1338832b4618dd6d424cdf674f02d9e2
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-07 16:54:33 +0530

    Update char_equilateral.py

--- Patterns/char_equilateral.py
+++ Patterns/char_equilateral.py
@@ -1,7 +1,12 @@
 print("Print equilateral triangle Pyramid with characters")
-s = 5
+num = int(input("Please enter the number of rows: "))
+
 asciiValue = 65
-for rows in range(0 , 5):
-    for columns in range(0 , s-rows):
+for rows in range(0 , num):
+    for spaces in range(0 , num - rows - 1):
         print(end=" ")
-        
+    for columns in range(0 , rows + 1):
+            alphabet = chr(asciiValue)
+            asciiValue += 1
+            print(alphabet, end=" ")
+    print()    
