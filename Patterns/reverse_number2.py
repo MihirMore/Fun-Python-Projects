3cd3f91d6cbfc44921a03ea6596f848a8422bba2
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-07 17:04:59 +0530

    Update reverse_number2.py

--- Patterns/reverse_number2.py
+++ Patterns/reverse_number2.py
@@ -9,4 +9,7 @@
 '''
 num = int(input("Please enter the number of rows: "))
 
-for rows in range()
+for rows in range(0 , num):
+    for columns in range(rows + 1 , 0 , -1):
+        print(columns , end=" ")
+    print()    
