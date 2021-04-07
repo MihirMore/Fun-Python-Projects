67731858ffe806d74a9857da6f4b8985fee244e6
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-07 16:27:52 +0530

    Create char_equilateral.py

---
+++ Patterns/char_equilateral.py
@@ -0,0 +1,7 @@
+print("Print equilateral triangle Pyramid with characters")
+s = 5
+asciiValue = 65
+for rows in range(0 , 5):
+    for columns in range(0 , s-rows):
+        print(end=" ")
+        
