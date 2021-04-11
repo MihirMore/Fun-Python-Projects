f9dbe31bc7cc15d44023b2c565ba3ca0e713cb17
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-11 18:22:53 +0530

    Create Pattern23.py

---
+++ Patterns/Star-Pattern/Pattern23.py
@@ -0,0 +1,36 @@
+## Pattern - 23
+'''
+Example
+
+Input
+
+Input N: 5
+Output
+
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      **
+***    ***
+****  ****
+**********
+'''
+
+rows = int(input("Please enter the number of rows: "))
+space = rows-2
+star = 1
+for i in range(0 , rows*2):
+    for spaces in range(space*2 , -1, -1):
+        print("*", end=" ")        
+    for stars in range(1 ,star*2):
+        print(" " , end=" ")        
+    if (i < rows-1):
+        star += 1
+        space -= 1
+    else:
+        star -= 1
+        space += 1      
+    print()                                 
