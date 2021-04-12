c90b9555564eee526fd8e2d6730dddbd69ed742c
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-04-12 21:22:10 +0530

    Update Pattern24.py

--- Patterns/Star-Pattern/Pattern24.py
+++ Patterns/Star-Pattern/Pattern24.py
@@ -17,3 +17,17 @@ Output
   ****
 *****
 '''
+rows = int(input("Please enter the number of rows: "))
+
+for i in range(0 , rows):
+    for j in range(0 , 2*i):
+        print(" ", end="")
+    for k in range(rows-i, 0 , -1):
+        print("*" , end="")
+    print()
+for m in range(1, rows):    
+    for n in range((rows*2)-2*m-2,0 , -1):
+        print(" ", end="")
+    for o in range(0,m+1):
+        print("*", end="")                          
+    print()
