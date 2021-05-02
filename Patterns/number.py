165145e0656e88bac0ec85164ab6d6fa3930b34d
Mihir More <37392794+MihirMore@users.noreply.github.com>
2021-05-02 23:06:14 +0530

    Delete pattern

--- Patterns/number.py
+++
@@ -1,6 +0,0 @@
-number = int(input("Please enter the number of rows: "))
-
-for rows in range( number + 1):
-    for columns in range(rows):
-        print(rows,end=" ")
-    print()    
--- Patterns/simplepyramid.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Enter the number of rows to print: "))
-
-for i in range(0 , n):
-    for j in range(0 , i+1):
-        print("*",end="")
-    print()    
